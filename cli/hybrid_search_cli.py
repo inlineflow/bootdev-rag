b4eb457601cb6edd71349c74782fcf8d9bd63c05
Danil Samsonov <thrashdev@protonmail.com>
2025-11-05 16:15:06 +0600

    solved CH6:L3

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,15 +1,34 @@
 import argparse
+from itertools import repeat
 
+def normalize(values: list[float]) -> list[float]:
+    if len(values) == 0:
+        return []
+    high = max(values)
+    low = min(values)
+    if high == low:
+        return list(repeat(1.0, len(values)))
 
+    k = high - low
+    result = [(score - low) / k for score in values]
+    return result
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
-    parser.add_subparsers(dest="command", help="Available commands")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    normalize_cmd = subparsers.add_parser("normalize")
+    normalize_cmd.add_argument("values", type=float, nargs="+")
 
     args = parser.parse_args()
 
     match args.command:
+        case "normalize":
+            values = normalize(args.values)
+            for score in values:
+                print(f"* {score:.4f}")
+
         case _:
             parser.print_help()
 
